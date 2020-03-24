5792a9a584a0b8c9561e5fe85120f6e9c11b41a0
Artem Kostiuk <postatum@gmail.com>
2020-03-24 10:56:39 +0200

    Freeze htmls again

--- build.sh
+++ build.sh
@@ -8,15 +8,14 @@ popd
 ## Generate global JSON-LD model
 pushd ./scripts
 npm i
+npm link ../../aml2doc
 npm run global_model
 popd
 
 ## HTML documentation
 rm -rf html/*
 mkdir -p html
-pushd ./scripts/node_modules/@aml-org/aml2html
-npm run aml2html -- ../../../../html -d ../../../../src -g ../../../cfg.js -t ../../../templates
-popd
+node ./scripts/node_modules/@aml-org/aml2html ./html -d ./src -g ./scripts/cfg.js -t ./scripts/templates
 cp -rf ./scripts/templates/*  ./html/
 rm -rf ./html/*.mustache
 
